8898a0a72f25352145803d2dcb6ad847936f6172
Edward Safo <edsafony@gmail.com>
2023-03-05 14:50:08 -0500

    added image plugin through tutorial #3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+    <Layout pageTitle="Home - Safo Foundation">
+      <p>Bringing opportunity to West Africa through education scholarship.</p>
+      <StaticImage
+        alt="Dr. Safo at her office"
+        src="../images/Margaret Safo_Office_Cropped.jpg"
+      />
+
     </Layout>
   )
 }
